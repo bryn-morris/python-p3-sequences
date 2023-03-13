8b3a96914c442f1d04a3f7d38465f1179813f6b6
Brynfor Morris <brynfor.morris@gmail.com>
2023-03-13 11:01:30 -0500

    first pass at solving fibonacci sequence

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+
+    fib_list = [0,1]
+
+    if (length == 0):
+        fib_list.clear()
+    elif (length == 1):
+        del(fib_list[1])
+    elif (length == 2):
+        fib_list
+    else:
+        i = 1
+        while i <= (length-2):
+            fib_list.append(fib_list[i]+fib_list[i-1])
+            i += 1 
+
+    print (fib_list)
+
